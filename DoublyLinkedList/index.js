0d347c922e21d3920a1de2c50868b7506743b95b
karu19961j <karu19961j@gmail.com>
2024-01-15 16:22:44 +0530

    adding DoublyLinkedList

---
+++ DoublyLinkedList/index.js
@@ -0,0 +1,155 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    // 1->2->null
+    let temp = this.tail.prev;
+    temp.next = null;
+    this.tail.prev = null;
+    this.tail = temp;
+    this.length--;
+    return this;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    // 2->1->null
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      // 2->1->null
+      this.head = this.head.next;
+      this.head.prev = null;
+      temp.next = null;
+    }
+    this.length--;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    let temp = this.head;
+    if (index < this.length / 2) {
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      for (let i = this.length - 1; i > index; i--) {
+        temp = temp.prev;
+      }
+    }
+    return temp;
+  }
+
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === 0) {
+      return this.unshift(value);
+    }
+    if (index === this.length) {
+      return this.push(value);
+    }
+    //1->2->3->10->4->5, newNode = 10, index = 3
+    const newNode = new Node(value);
+    const before = this.get(index - 1);
+    const after = before.next;
+    before.next = newNode;
+    newNode.prev = before;
+    newNode.next = after;
+    after.prev = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    if (index === 0) {
+      return this.shift();
+    }
+    if (index === this.length - 1) {
+      return this.pop();
+    }
+    let temp = this.get(index);
+    // 1->2->3->4, index = 1
+    // temp.prev.next = 2, temp.next = 3
+    if (temp) {
+      temp.prev.next = temp.next;
+      temp.next.prev = temp.prev;
+      temp.next = null;
+      temp.prev = null;
+      this.length--;
+      return true;
+    }
+    return false;
+  }
+}
+
+const myDoublyLinkedList = new DoublyLinkedList(1);
+myDoublyLinkedList.push(2);
+myDoublyLinkedList.push(3);
+myDoublyLinkedList.remove(1);
+
+console.log("🚀 ~ myDoublyLinkedList:", myDoublyLinkedList);
